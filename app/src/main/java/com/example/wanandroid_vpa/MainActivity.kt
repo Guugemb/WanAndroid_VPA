8b3aca8ab9ff043449be4445f878b28ec366ff9e
= <=>
2020-11-27 15:59:12 +0800

    First Commit Test

--- app/src/main/java/com/example/wanandroid_vpa/MainActivity.kt
+++ app/src/main/java/com/example/wanandroid_vpa/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.wanandroid_vpa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG, "onCreate: ")
+    }
+
+    companion object {
+        const val TAG = "zhujin"
     }
 }
